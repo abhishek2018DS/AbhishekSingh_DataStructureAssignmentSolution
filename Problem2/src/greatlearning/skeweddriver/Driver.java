6c730c4dc70fc435cd53c6b7a8eaae611fa96093
abhishek2018DS <42770193+abhishek2018DS@users.noreply.github.com>
2022-02-23 08:47:10 +0530

    Update Driver.java

--- Problem2/src/greatlearning/skeweddriver/Driver.java
+++ Problem2/src/greatlearning/skeweddriver/Driver.java
@@ -1,25 +1,21 @@
-package com.greatlearning.skeweddriver;
-import com.greatlearning.rightscrew.SkewedRightTree;
+package greatlearning.skeweddriver;
+import greatlearning.rightscrew.SkewedRightTree;
+import greatlearning.node.Node;
 
 
-public class Driver
-{
-	// Driver Code
-	public static void main (String[] args)
-	{
-		
-		SkewedRightTree tree= new SkewedRightTree();
+public class Driver {
+	public static Node node;
+
+	public static void main(String[] args) {
+		SkewedRightTree tree = new SkewedRightTree();
 		tree.node = new Node(50);
 		tree.node.left = new Node(30);
 		tree.node.right = new Node(60);
-		tree.node.left.left= new Node(10);
-		tree.node.right.left= new Node(55);
-		// Order of the Skewed tree can be defined as follows -
-		// For Increasing order - 0
-		// For Decreasing order - 1
-		int order = 0;
-		tree.flattenSkewedTree(Node root, int order);
-		tree.traverseRightSkewed(tree.headNode);
-		}
-		
+		tree.node.left.left = new Node(10);
+		tree.node.left.right = new Node(40);
+
+		tree.convertBSTToSkewedTree(tree.node);
+		tree.traverseTree(tree.headNode);
+
+	}
 }
