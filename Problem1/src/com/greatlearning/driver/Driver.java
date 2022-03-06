8b51b06df08a693092d6e06fe36818dc0041501e
abhishek2018DS <42770193+abhishek2018DS@users.noreply.github.com>
2022-03-06 22:05:08 +0530

    Create Driver.java

---
+++ Problem1/src/com/greatlearning/driver/Driver.java
@@ -0,0 +1,11 @@
+package com.greatlearning.driver;
+import com.greatlearning.skyscraper.Skyscraper_floor_building;
+
+public class Driver {
+	public static void main(String[] args) {
+		Skyscraper_floor_building skyscraper = new Skyscraper_floor_building();
+		skyscraper.skyscraper_implementation();
+	}
+
+
+}
