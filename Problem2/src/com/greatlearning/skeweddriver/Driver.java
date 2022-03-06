70e73e8c938feac5a465cf094c2ae6a1f5b5cd3b
abhishek2018DS <42770193+abhishek2018DS@users.noreply.github.com>
2022-03-06 22:40:05 +0530

    Create Driver.java

---
+++ Problem2/src/com/greatlearning/skeweddriver/Driver.java
@@ -0,0 +1,21 @@
+package com.greatlearning.skeweddriver;
+import com.greatlearning.rightskewed.SkewedRightTree;
+import com.greatlearning.nodeclass.Node;
+
+
+public class Driver {
+	public static Node node;
+
+	public static void main(String[] args) {
+		SkewedRightTree tree = new SkewedRightTree();
+		tree.node = new Node(50);
+		tree.node.left = new Node(30);
+		tree.node.right = new Node(60);
+		tree.node.left.left = new Node(10);
+		tree.node.left.right = new Node(40);
+
+		tree.convertBSTToSkewedTree(tree.node);
+		tree.traverseTree(tree.headNode);
+
+	}
+}
