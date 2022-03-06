be02c893abba35940ca3d6b539d0da6223dc0d70
abhishek2018DS <42770193+abhishek2018DS@users.noreply.github.com>
2022-03-06 22:38:39 +0530

    Create SkewedRightTree.java

---
+++ Problem2/src/com/greatlearning/rightskewed/SkewedRightTree.java
@@ -0,0 +1,42 @@
+package com.greatlearning.rightskewed;
+import com.greatlearning.nodeclass.Node;
+
+
+public class SkewedRightTree
+{
+	public static Node node, prevNode = null, headNode = null;
+
+	// to convert BST to Skewed Tree
+	public void convertBSTToSkewedTree(Node root) {
+		// check for null tree
+		if (root == null) {
+			return;
+		}
+
+		// use recursion
+		convertBSTToSkewedTree(root.left);
+		Node rightNode = root.right;
+
+		if (headNode == null) {
+			headNode = root;
+		} else {
+			prevNode.right = root;
+		}
+		root.left = null;
+		prevNode = root;
+
+		// do recursion with rightNode
+		convertBSTToSkewedTree(rightNode);
+	}
+
+	// traverse the right skewed tree using recursion
+	public void traverseTree(Node root) {
+		if (root == null) {
+			return;
+		}
+
+		System.out.print(root.value + " ");
+		traverseTree(root.right);
+	}
+
+}
